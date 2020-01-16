3bf7c0bc790922bcdc774207b2494e63a1586984
User configured by VIRL Configuration Engine 0.23.12 <cisco@guestendpoint-pyats-topology-v1-zdsyyc-jumphost.novalocal>
2020-01-16 15:48:28 +0000

    step1 pyats testcase added

---
+++ step1_pyats_example.py
@@ -0,0 +1,47 @@
+#!/bin/env python
+
+# To get a logger for the script
+import logging
+
+from pyats import aetest
+from pyats.log.utils import banner
+
+# Genie Imports
+from genie.conf import Genie
+
+# Get your logger for your script
+log = logging.getLogger(__name__)
+
+class common_setup(aetest.CommonSetup):
+
+
+    @aetest.subsection
+    def establish_connections(self,testbed):
+        genie_testbed = Genie.init(testbed)
+        self.parent.parameters['testbed'] = genie_testbed
+        device_list = []
+        for device in genie_testbed.devices.values():
+            log.info(banner(
+                "Connect to device '{d}'".format(d=device.name)))
+            try:
+                device.connect()
+            except Exception as e:
+                self.failed("Failed to establish connection to '{}'".format(
+                    device.name))
+            device_list.append(device)
+        # Pass list of devices the to testcases
+        self.parent.parameters.update(dev=device_list)
+
+if __name__ == '__main__': # pragma: no cover
+
+    import argparse
+    from pyats.topology import loader
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--testbed', dest = 'testbed',
+                        type = loader.load)
+
+    args, unknown = parser.parse_known_args()
+
+    aetest.main(**vars(args))
+
