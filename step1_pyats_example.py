57b38602448c099623c0178ca50022500bb51b51
User configured by VIRL Configuration Engine 0.23.12 <cisco@guestendpoint-pyats-topology-v1-zdsyyc-jumphost.novalocal>
2020-01-16 21:01:51 +0000

    midnigh-save

--- step1_pyats_example.py
+++ step1_pyats_example.py
@@ -29,8 +29,54 @@ class common_setup(aetest.CommonSetup):
                 self.failed("Failed to establish connection to '{}'".format(
                     device.name))
             device_list.append(device)
-        # Pass list of devices the to testcases
+        # Pass list of devices to testcases
         self.parent.parameters.update(dev=device_list)
+
+
+class PingTestcase(aetest.Testcase):
+
+    @aetest.setup
+    def setup(self):
+
+
+      dest_ips = [] # list to store all IPs from topology
+
+      for device in self.parent.parameters['dev']:
+        for intf in device:
+          print(f'{device.name}:{intf.name}:{intf.ipv4}')      
+
+          
+          dest_ips.append(str(intf.ipv4))
+
+          result = self.parent.parameters['testbed'].devices['nx-osv-1'].ping('8.8.8.8')
+          print(result)
+
+
+    @aetest.test
+    def ping(self):
+
+
+      nx = self.parent.parameters['testbed'].devices['nx-osv-1']
+      csr = self.parent.parameters['testbed'].devices['csr1000v-1']
+
+      dest_links = csr.find_links(nx)
+      
+      for intf in csr.interfaces:
+ #stopped here
+        if intf.name in 
+          dest_ip.append(str(intf.ipv4.ip))
+        
+
+
+
+      try:
+            # store command result for later usage
+            result =  self.parent.parameters['testbed'].devices['nx-osv-1'].ping(destination)
+
+
+
+
+
 
 if __name__ == '__main__': # pragma: no cover
 
