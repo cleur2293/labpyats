48d03b6798689df1ff7230a5723be8a7a775f1c3
cleur2293 <cleur2293@protonmail.com>
2020-01-23 08:58:05 -0800

    minor final changes

--- step0_pyats_example.py
+++ step0_pyats_example.py
@@ -5,14 +5,8 @@ from genie.conf import Genie
 from os import path
 from os import mkdir
 
-# To handel errors with connections to devices
+# To handle errors with connections to devices
 from unicon.core import errors
-
-log = logging.getLogger(__name__)
-log.level = logging.INFO
-
-testbed = './pyats_testbed.yaml'
-testbed = Genie.init(testbed)
 
 
 def create_non_existing_dir(dir_path):
@@ -21,7 +15,7 @@ def create_non_existing_dir(dir_path):
             mkdir(dir_path)
         except PermissionError as e:
             log.error(f'Unable to create directory: {dir_path}. Insufficient privileges. Error: {e}')
-
+            exit(1)
 
 def write_commands_to_file(abs_filename, command_output):
     try:
@@ -30,12 +24,14 @@ def write_commands_to_file(abs_filename, command_output):
 
     except IOError as e:
         log.error(f'Unable to write output to file: {abs_filename}. Due to error: {e}')
+        exit(1)
 
     except PermissionError as e:
         log.error(f'Unable to write output to file: {abs_filename}. Insufficient privileges. Error: {e}')
+        exit(1)
 
 
-def collect_device_commands(commands_to_gather, dir_name):
+def collect_device_commands(testbed, commands_to_gather, dir_name):
     abs_dir_path = path.join(path.dirname(__file__), dir_name)
 
     create_non_existing_dir(abs_dir_path)
@@ -47,13 +43,13 @@ def collect_device_commands(commands_to_gather, dir_name):
         create_non_existing_dir(device_path)
 
         try:
-            device.connect(stdout=False)
+            device.connect()
         except errors.ConnectionError:
             log.error(f'Failed to establish connection to: {device.name}. Check connectivity and try again.')
             continue
 
         device.connectionmgr.log.setLevel(logging.ERROR)
-        # device.log_user(enable=False)
+        device.log_user(enable=True)
 
         if commands_to_gather.get(device_os):
             for command in commands_to_gather[device_os]:
@@ -66,19 +62,27 @@ def collect_device_commands(commands_to_gather, dir_name):
                 write_commands_to_file(abs_filename, command_output)
         else:
             log.error(f'No commands for operating system: {device_os} of device: {device_name} has been defined. This device has been skipped. Specify list of commands for {device_os} and try again.')
+            continue
 
 
 def main():
+    global log
+    log = logging.getLogger(__name__)
+    log.level = logging.INFO
+
+    testbed_filename = './pyats_testbed.yaml'
+    testbed = Genie.init(testbed_filename)
+
     commands_to_gather = {
-        'asa': ['show inventory', 'show running-config', 'show route', 'show ospf neighbor', 'show license all'],
-        'iosxe': ['show inventory', 'show running-config', 'show ip route vrf *', 'show ip ospf neighbor',
-                       'show license feature'],
+            'asa': ['show inventory', 'show running-config', 'show route', 'show ospf neighbor', 'show license all'],
+            'iosxe': ['show inventory', 'show running-config', 'show ip route vrf *', 'show ip ospf neighbor',
+                           'show license feature'],
         'nxos': ['show inventory', 'show running-config', 'show ip route vrf all', 'show ip ospf neighbor vrf all',
                        'show license usage']}
 
     dir_name = 'gathered_commands'
 
-    collect_device_commands(commands_to_gather, dir_name)
+    collect_device_commands(testbed, commands_to_gather, dir_name)
 
 
 if __name__ == '__main__':
