b50901db03e6b1adaa1331c5d37c7c114b4b9020
cleur2293 <cleur2293@protonmail.com>
2020-04-29 10:01:08 -0700

    added final scripts for sample parser

---
+++ asa_parser_check.py
@@ -0,0 +1,26 @@
+from genie.conf import Genie
+from genie import parsergen
+
+from pprint import pprint
+
+import logging
+
+testbed = Genie.init('pyats_testbed.yaml')
+
+asa = testbed.devices['asav-1']
+
+asa.connect()
+asa.connectionmgr.log.setLevel(logging.ERROR)
+
+show_counters = asa.device.execute('show counters')
+
+print(f'command output: \n{show_counters}')
+
+header = ['Protocol', 'Counter', 'Value', 'Context']
+
+result = parsergen.oper_fill_tabular(device_output=show_counters, device_os='asa', header_fields=header, index=[0, 1])
+
+print('result:')
+pprint(result.entries)
+
+
--- asaops.py
+++ asaops.py
@@ -22,10 +22,12 @@ class ShowCountersSchema(MetaParser):
 
     schema = {
         Any():{
+            Any():{
                 'Protocol': str,
                 'Counter': str,
                 'Value': str,
-                'Context': str
+                'Context': str,
+                },
            },
         }
 
@@ -41,8 +43,8 @@ class ShowCounters(ShowCountersSchema):
 
         header = ['Protocol', 'Counter', 'Value', 'Context']
 
-        result = parsergen.oper_fill_tabular(device_output=output, device_os='asa', header_fields=header, index=[1])
-
+        result = parsergen.oper_fill_tabular(device_output=output, device_os='asa', header_fields=header, index=[0, 1])
+        pprint(result.entries)
         return result.entries
 
 
@@ -52,13 +54,13 @@ class HealthASA(Base):
     def learn(self, custom=None):
 
         # Capture output from show counters parser
-        src = '[(?P<counter_name>.*)]'
-        dest = 'info[(?P<counter_name>.*)]'
-        req_keys = ['[Protocol]','[Counter]','[Value]','[Context]']
+        src = '[(?P<protocol_name>.*)][(?P<counter_name>.*)]'
+        dest = 'info[(?P<protocol_name>.*)][(?P<counter_name>.*)]'
+        req_keys = ['Value']
         for key in req_keys:
             self.add_leaf(cmd=ShowCounters,
-                          src=src + '[{}]'.format(key),
-                          dest=dest + '[{}]'.format(key))
+                          src=src + f'[{key}]',
+                          dest=dest + f'[{key}]')
 
         #Add ops data to the HealthASA ojbect
         self.make()
