7d786fb2787de1f10767bf62c2a66c5a717524d1
cleur2293 <cleur2293@protonmail.com>
2021-03-26 15:01:49 -0700

    Updated by asazhin

--- task10_runtestsjob.py
+++ task10_runtestsjob.py
@@ -4,7 +4,7 @@ from ats.easypy import run
 
 def main():
     # Find the location of the script in relation to the job file
-    ping_tests = os.path.join('./task10_labpyats.py')
+    ping_tests = os.path.join('./task9_labpyats.py')
 
     # Execute the testscript
     run(testscript=ping_tests)
