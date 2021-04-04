0f3f740424b11eb9af76fd0baa75bc2a6fa027ec
sesazhin <sesazhin@cisco.com>
2021-04-04 16:44:35 -0700

    added guide download code to .rungit.py

--- dr/.rungit.py
+++ dr/.rungit.py
@@ -1,44 +1,25 @@
 #!/usr/bin/env python3.7
 
 import logging
-import logging.handlers
 import os
 import shlex
+import shutil
 import subprocess
 import sys
 
+global logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
-def init_logging() -> None:
-    global logger
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-
-    log_formatter_console = logging.Formatter('%(filename)s: %(message)s')
-    format = '%(asctime)s %(levelname)s %(filename)s(%(lineno)d) %(message)s'
-    log_formatter_file = logging.Formatter(format)
-
-    console = logging.StreamHandler()
-    console.setFormatter(log_formatter_console)
-    console.setLevel(logging.INFO)
-
-    file_handler = \
-        logging.handlers.RotatingFileHandler('/var/tmp/pyats_lab.log',
-                                             maxBytes=(1048576 * 10),
-                                             backupCount=10,
-                                             encoding='utf-8')
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(log_formatter_file)
-
-    logger.addHandler(console)
-    logger.addHandler(file_handler)
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+logger.addHandler(console)
 
 
 def main(dir: str) -> None:
     devnull = open(os.devnull, 'w')
 
-    command_run = \
-        shlex.split(f'git clone '
-                    f'https://github.com/cleur2293/labpyats.git {dir}')
+    command_run = shlex.split(f'git clone https://github.com/cleur2293/labpyats.git {dir}')
 
     logger.debug(f'running command: {command_run}')
 
@@ -46,23 +27,30 @@ def main(dir: str) -> None:
 
     if code:
         if code == 128:
-            logger.error(f'Unable to create Git Repo in directory: '
-                         f'{dir}. This directory already exists.')
+            logger.error(f'Unable to create Git Repo in directory: {dir}. This directory already exists.')
         else:
-            logger.error(f'Unable to coplete "git clone". '
-                         f'Unexpected error has happened')
+            logger.error(f'Unable to coplete "git clone" some error has happened')
     else:
-        logger.info(f'git clone has finished successfully. '
-                    f'Repo has been created in {dir}')
+        logger.info(f'git clone has finished successfully. Repo has been created in {dir}')
 
+        lab_guide_name = 'Lab_Guide.docx'
+        lab_guide_path = os.path.join(dir, 'dr', lab_guide_name)
+        lab_guide_move = os.path.join('/mnt/c/Users/Administrator/Desktop/', lab_guide_name)
 
-if __name__ == '__main__':
-    init_logging()
+        if os.path.exists(lab_guide_path):
+            logger.info(f'Lab guide found')
+            shutil.move(lab_guide_path, lab_guide_move)
+            logger.info(f'Lab guide moved')
+        else:
+            logger.error(f'Lab guide not found')
+
+if __name__ ==  '__main__':
     if len(sys.argv) == 2:
         dir = sys.argv[1]
         logger.debug(f'dir: {dir}')
         main(dir)
     else:
-        logger.error(f'Incorrect number of arguments passed. '
-                     f'Expected 1 got: {len(sys.argv)-1}')
+        logger.error(f'Incorrect number of arguments passed. Expected 1 got: {len(sys.argv)-1}')
         exit(1)
+
+
